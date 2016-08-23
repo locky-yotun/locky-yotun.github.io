8d692f3bcb871f0a13c98ec65838f80757d6529f
Maxim Zaytsev <m.zaytsev@superjob.ru>
2016-08-23 19:25:31 +0300

    qqq

---
+++ script.js
@@ -0,0 +1,78 @@
+(function(){
+	angular.module('app', []);
+
+})();
+
+(function(){
+	angular.module('app').factory('CachableItems', CachableItems);
+
+	CachableItems.$inject = [];
+
+	function CachableItems(){
+		return {
+			programmingLangs: {
+				name: 'programmingLangs',
+				url: './back/langs.json',
+				requiredVersion: '1.0'
+			}
+		}
+	}
+})();
+
+(function(){
+	angular.module('app').service('LangsService', LangsService);
+
+	LangsService.$inject = ['CachableItems', '$q', '$http'];
+
+	function LangsService(CachableItems, $q, $http){
+		var service = {};
+
+		service.getItems = getItems;
+
+		return service;
+
+		var res = CachableItems.programmingLangs,
+			items = [];
+
+		if (localStorage.getItem(res.name)) {
+			try {
+				items = JSON.parse(localStorage.getItem(res.name));
+			} catch () {}
+		}
+
+		function getItems(){
+			var defer = $q.defer();
+
+			if (items.length) {
+				refer.resolve(items);
+			} else {
+				$http({
+					method: 'get',
+					url: res.url
+				}).then(function(response){
+					defer.resolve(response.data);
+				}).catch(function(reason){
+					defer.reject(reason);
+				});
+			}
+
+			return defer.promise;
+		}
+	}
+})();
+
+(function(){
+	angular.module('app').controller('TestController', TestController);
+
+	TestController.$inject = ['LangsService'];
+
+	function TestController(LangsService){
+		var vm = this;
+
+		vm.items = [];
+
+		LangsService.getItems().then(function(items){
+			vm.items = items;
+		});
+	}
+})();
